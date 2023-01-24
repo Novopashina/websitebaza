a93a54f22b5727bc94d0012339a6da23274db885
Ksenyakin <30597080+ksenyakin@users.noreply.github.com>
2023-01-24 15:44:39 +0300

    Добавление отзывов

--- js/script.js
+++ js/script.js
@@ -21,4 +21,26 @@ $(document).ready(function () {
         $('.plans__card-business').removeClass('business-scale');
     });
 
+
+    // ОТЗЫВЫ
+
+    $('.reviews__slider').on(`init reInit`, function (event, slick) {
+        $('.slick-slide-num-current').text('0' + 1 + ' '); // ' / ' + '0' + slick.slideCount
+    })
+    $('.reviews__slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+        $('.slick-slide-num-current').text('0' + (currentSlide + 1) + ' '); // + ' / 0' + slick.slideCount
+    })
+    $('.reviews__slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        adaptiveHeight: true,
+        easing: 'ease',
+        appendArrows: '.slick__arrows',
+        prevArrow: '<span class="slick-prev"><</span>',
+        nextArrow: '<span class="slick-next">></span>'
+
+    });
 });
