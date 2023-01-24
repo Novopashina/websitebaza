0c02679f3d6d245cd0c4e29c0bb08b43b605ab44
novopashina <112856474+novopashina@users.noreply.github.com>
2023-01-24 15:47:53 +0300

    партнеры

--- js/script.js
+++ js/script.js
@@ -43,4 +43,48 @@ $(document).ready(function () {
         nextArrow: '<span class="slick-next">></span>'
 
     });
+    // ПАРТНЕРЫ
+
+    $('.partners__slider-1').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        easing: 'ease',
+        autoplay: true,
+        pauseOnFocus: true,
+        autoplaySpeed: 2500,
+        speed: 1500,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: true,
+            }
+        }],
+    });
+
+    $('.partners__slider-2').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        easing: 'ease',
+        autoplay: true,
+        pauseOnFocus: true,
+        autoplaySpeed: 2700,
+        speed: 2000,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                focusOnSelect: true,
+                variableWidth: true
+            }
+        }],
+    });
 });
